ae936e9cc174c1911df68fa47985033933b47098
nikolajhm7 <nikolajhoejer7@gmail.com>
2025-09-29 22:10:09 +0200

    chore: improvements to form

--- Handin1/src/app/app.routes.ts
+++ Handin1/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { Home } from './components/screens/home/home'
 import { Transactions } from './components/screens/transactions/transactions';
 import { AddCreditCard } from './components/screens/add-credit-card/add-credit-card';
-import { CreditCardDetails } from './components/screens/credit-card-details/credit-card-details';
 
 export const routes: Routes = [
     {path: '', component: Home, title: 'Home'},
     {path: 'transactions', component: Transactions, title: 'Transactions'},
-    {path: 'cards/:cardNumber', component: CreditCardDetails, title: 'Card Details'},
+    {   path: 'cards/:cardNumber', 
+        loadComponent: () => import('./components/screens/credit-card-details/credit-card-details')
+            .then(m => m.CreditCardDetails), 
+        title: 'Card Details'}, // Lazy load
     {path: 'add', component: AddCreditCard, title: 'Add Card'},
     {path: '**', redirectTo: ''} // udelukkende til at redirect alle ukendte paths til home
 ];
